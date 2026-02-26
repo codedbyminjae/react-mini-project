caec8641bee4bcce3450980d79c12e6bb3ed3850
김민재 <minjaekim7111@naver.com>
2026-02-26 16:54:48 +0900

    feat: implement todo completion toggle functionality

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -39,11 +39,24 @@ function App() {
 
     setTodos([newTodo, ...todos]);
   };
+
+  // todos State의 값들 중에서
+  // targetId와 일치하는 id를 갖는 투두 아티엠의 isDone 변경
+
+  // 인수: todos 배열에서 targetId와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+  const onUpdate = (targetId) => {
+    setTodos((prev) =>
+      prev.map((todo) =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo,
+      ),
+    );
+  };
+
   return (
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List todos={todos} />
+      <List todos={todos} onUpdate={onUpdate} />
     </div>
   );
 }
