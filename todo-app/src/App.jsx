92818cba3754a4e4d15236cd1f7064eaf89e7de5
김민재 <minjaekim7111@naver.com>
2026-02-26 16:15:15 +0900

    feat: implement enter key submission with empty input focus handling

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,16 +1,49 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Header from "./components/Header";
 import List from "./components/List";
 import Editor from "./components/Editor";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "지원이 만나기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "샤브올데이 가기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
+  };
   return (
     <div className="App">
       <Header />
-      <Editor />
-      <List />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} />
     </div>
   );
 }
