6f5c4a74dc2dcc060cb15a225480a0d23c588820
codedbyminjae <minjaekim7111@naver.com>
2026-03-19 09:51:41 +0900

    feat: Diary 페이지 구현 (Nav & UI)

---
+++ emotion-diary/src/util/get-stringed-date.js
@@ -0,0 +1,14 @@
+export const getStringedDate = (targetDate) => {
+  // 날짜 -> yyyy-mm-dd 형태의 문자열로 반환
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+  return `${year}-${month}-${date}`;
+};
